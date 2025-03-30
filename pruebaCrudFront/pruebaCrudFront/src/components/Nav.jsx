df09cf45c96e2436f7d91bcec56af85d3af2215c
joel-briones-B-Low <joellbriones701@gmail.com>
2025-03-29 19:24:23 -0600

    se agrega mejora de experiencia del usuario

--- pruebaCrudFront/pruebaCrudFront/src/components/Nav.jsx
+++ pruebaCrudFront/pruebaCrudFront/src/components/Nav.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 export default function Nav({onSearch}){
+
+    //estado para el buscador 
+    const [searchID, setSearchId] = useState("");
+
+
 
     const handlerBuscar = (e) =>{
         e.preventDefault();
         //davegar por el DOM
         const id = e.target.elements.searchId.value;
         onSearch(id);
+        //limpiar el campo
+        setSearchId('')
+    }
+
+    const habdlerTraerPersonas = () =>{
+        setSearchId('')
+        // al momneto de traer las personas el promp se pasa nulo para que no haya un error de que
+        // cuando le pones algo al input de buscar y traes personas, la tabla se vuelve dependiete del mismo promp
+
+        onSearch(null)
     }
 
     return (
@@ -21,14 +37,20 @@ export default function Nav({onSearch}){
             <div className="collapse navbar-collapse " id="navbarContent">
                 <ul className="navbar-nav me-auto">
                     <li className="nav-item">
-                        <Link className="nav-link fs-4" to="/">traer personas</Link>
+                        <Link className="nav-link fs-4" to="/"
+                        onClick={habdlerTraerPersonas}>traer personas</Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link fs-4" to="/agregar">Agregar Persona</Link>
                     </li>
                 </ul>
                 <form className="d-flex" role="search" onSubmit={handlerBuscar}>
-                <input className="form-control me-2" type="search" placeholder="ID persona" name="searchId" aria-label="Search"/>
+                <input className="form-control me-2" type="search" placeholder="ID persona" name="searchId" aria-label="Search"
+                value={searchID}
+                //cambio
+                onChange={(e) => setSearchId(e.target.value)} // actualizar el estado al cambiar el input de valor
+                />
+
                 <button className="btn btn-outline-success" type="submit">Buscar</button>
                   </form>
             </div>
